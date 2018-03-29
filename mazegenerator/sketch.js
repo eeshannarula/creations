ef2aad65b7452a3d6e8e1145875a1a3c618c6f55
Eeshan <33724475+eeshannarula@users.noreply.github.com>
2018-03-29 22:06:26 +0530

    maze generator ..

---
+++ mazegenerator/sketch.js
@@ -0,0 +1,106 @@
+function make2dArray(cols,rows)
+{
+	var arr=new Array(cols);
+	for (var i = 0; i < arr.length; i++) {
+		arr[i]=new Array(rows);
+		}
+		return arr;
+}
+
+var grid;
+var grid2;
+var cols;
+var rows;
+var current;
+
+function setup() {
+	createCanvas(400, 400);
+	cols=width/40;
+	rows=height/40;
+	grid=make2dArray(cols,rows)
+
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j <rows; j++) {
+			grid[i][j]=new cell(i,j);
+		}
+	}
+
+current=grid[0][0];
+
+
+
+
+}
+
+function draw() {
+         background(51);
+				 frameRate(5);
+
+				 for (var i = 0; i < cols; i++) {
+					 for (var j = 0; j <rows; j++) {
+						 grid[i][j].show();
+						 if(current==grid[i][j])
+						 {
+							 grid[i][j].current=true;
+						 }
+						 else{
+							 grid[i][j].current=false;
+						 }
+					 }
+				 }
+}
+
+function cell(i,j)
+{
+	   /*
+       top-right-bottom-left
+			 top   =[0];
+			 right =[1];
+			 bottom=[2];
+			 left  =[3];
+		 */
+	   this.edges=[true,true,true,true]
+     this.i=i;
+		 this.j=j;
+		 this.side=40;
+		 this.x=this.i*this.side;
+		 this.y=this.j*this.side;
+		 this.current=false;
+		 this.visited=false;
+
+		 this.show=function()
+		 {
+			 noFill();
+			 stroke(255)
+			 strokeWeight(4);
+			if(this.edges[0]==true)
+			{
+				line(this.x,this.y,this.x+this.side,this.y)
+			}
+			if(this.edges[1]==true)
+			{
+				line(this.x+this.side,this.y,this.x+this.side,this.y+this.side)
+			}
+			if(this.edges[2]==true)
+			{
+				line(this.x,this.y+this.side,this.x+this.side,this.y+this.side)
+			}
+			if(this.edges[3]==true)
+			{
+				line(this.x,this.y,this.x,this.y+this.side)
+			}
+
+       if(this.current)
+			 {
+				 fill(255,100,0)
+				 rect(this.x,this.y,this.side,this.side);
+				 this.visited=true;
+			 }
+
+		 }
+
+		 this.checkNeighbours=function()
+		 {
+			 //chek surroundins...
+		 }
+}
