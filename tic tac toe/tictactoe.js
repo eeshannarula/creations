80ff34e82259ecc6c825c9adbd6ecb14cc23859f
Bangyen Pham <66754151+bangyen@users.noreply.github.com>
2020-07-04 16:00:45 -0400

    Update tictactoe.js

--- tic tac toe/tictactoe.js
+++ tic tac toe/tictactoe.js
@@ -1,36 +1,42 @@
-var score=1
-function mouse(event)
-{
-	event.target.setAttribute("disabled", "");
-	console.log('clicked')
-    nextmove(event)
-    if(score%2==0)
-    {event.target.value='x';
-     event.target.style.color = "blue";
-    }
-    else
-    {
-	event.target.value='o';
-	event.target.style.color = "red";
-    }
+var score = 1
+
+function mouse(event) {
+    event.target.setAttribute("disabled", "");
+    console.log('clicked');
+    nextmove(event);
+    
+    let parity = score % 2;
+
+    event.target.value = ['x', 'o'][parity];
+    event.target.style.color = ['blue', 'red'][parity];
+
     check()
 }
-function check(){
 
-if(form.btn1.value==form.btn2.value && form.btn2.value==form.btn3.value){alert('win');location.reload()}
-if(form.btn1.value==form.btn4.value && form.btn4.value==form.btn7.value){alert('win');location.reload()}
-if(form.btn1.value==form.btn5.value && form.btn5.value==form.btn9.value){alert('win');location.reload()}
-if(form.btn1.value==form.btn2.value && form.btn2.value==form.btn3.value){alert('win');location.reload()}
-if(form.btn1.value==form.btn2.value && form.btn2.value==form.btn3.value){alert('win');location.reload()}
-if(form.btn2.value==form.btn5.value && form.btn5.value==form.btn8.value){alert('win');location.reload()}
-if(form.btn1.value==form.btn2.value && form.btn2.value==form.btn3.value){alert('win');location.reload()}
-if(form.btn3.value==form.btn6.value && form.btn6.value==form.btn9.value){alert('win');location.reload()}
-if(form.btn3.value==form.btn5.value && form.btn5.value==form.btn7.value){alert('win');location.reload()}
-if(form.btn4.value==form.btn5.value && form.btn5.value==form.btn6.value){alert('win');location.reload()}
-if(form.btn7.value==form.btn8.value && form.btn8.value==form.btn9.value){alert('win');location.reload()}
-
+function check() {
+    let buttons = [
+        form.btn1.value, form.btn2.value, form.btn3.value,
+        form.btn4.value, form.btn5.value, form.btn6.value,
+        form.btn7.value, form.btn8.value, form.btn9.value
+    ]
+    
+    let conditions = [
+    	[1, 2, 3], [1, 4, 7], [1, 5, 9], [2, 5, 8],
+	[3, 6, 9], [3, 5, 7], [4, 5, 6], [7, 8, 9]
+    ]
+    
+    function compare(arr, x, y, z) {
+        return arr[x - 1] == arr[y - 1] == arr[z - 1];
+    }
+    
+    for (let item in conditions) {
+        if compare(buttons, ...item) {
+	    alert('win');
+	    location.reload();
+	}
+    }
 }
-function nextmove()
-{
-score++
+
+function nextmove() {
+    score++
 }
